0d8b3a7cd8e473e15b4cb89b77f42db0369d4632
Alex Tkachev <tkachev.alex@gmail.com>
2011-07-04 19:08:40 +0300

    progress bar widget including exmpale

---
+++ dist/jquery.progress-bar.pack.js
@@ -0,0 +1,13 @@
+/*
+* progressBar - jQuery framework extensions
+*
+* Version: 0.0.1a
+* Copyright 2011 Alex Tkachev
+*
+* Dual licensed under MIT or GPLv2 licenses
+*   http://en.wikipedia.org/wiki/MIT_License
+*   http://en.wikipedia.org/wiki/GNU_General_Public_License
+*
+* Date: Mon Jul 4 16:59:27 2011 +0300
+*/
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(4(a){4 d(a){3 b=[];o(3 c 13 a)b.1b({j:12(c,10),8:a[c]});b.X(4(a,b){a=a.j,b=b.j;6 a<b?-1:a===b?0:1});o(3 d=1;d<b.t;d++){3 e=b[d-1];e.p=b[d].j-1}b[b.t-1].p=n;6 b}4 c(b){6 a(\'<l s="Z-u-1c"><l s="u"/><l s="F"/></l>\').17(b)}3 b=4(){2.K.18(2,19)};a.z(b.L,{K:4(b,c){a.z(2,c),2.5=b.C("7",2.7),2.9=d(2.9),2.y(2.E)},y:4(a){2.v=a%(2.m+1),2.A()},M:4(a){2.m=a,2.y(2.v)},A:4(){3 b=J.D(2.v/2.m*n);I.O(b)&&(b=0);3 c=2.5.k(),d=2.5.q(),e=J.D(b/n*d);a(".u",2.5).q(e).k(c);3 f=a(".F",2.5);f.R(b+"%").k(c).8({"S-k":c+"r"});3 g=e-f.U(!0);g<0&&(g=0),f.8(2.7?{V:g+"r"}:{W:g+"r"});o(3 h=0;h<2.9.t;h++){3 i=2.9[h];2.5.C(i.8,i.p>=b&&i.j<=b)}}}),a.w.x=4(d){14(d){2.15(4(){3 e=a(2),f=a.z(a.w.x.B,d);f.7=e.8("P")=="7",e.H("G",Q b(c(e),f))});6 2}6 a(2).H("G")},a.w.x.B={E:0,m:n,9:{0:"16",N:"T",Y:"1a"},q:11}})(I)',62,75,'||this|var|function|el|return|rtl|css|cssRange||||||||||from|height|div|total|100|for|to|width|px|class|length|bar|value|fn|progressBar|setValue|extend|syncUI|defaults|toggleClass|round|start|percent|api|data|jQuery|Math|initialize|prototype|setTotal|30|isNaN|direction|new|html|line|orange|outerWidth|right|left|sort|75|progress||200|parseInt|in|if|each|red|appendTo|apply|arguments|green|push|container'.split('|'),0,{}))
---
+++ src/jquery.progress-bar.js
@@ -0,0 +1,104 @@
+(function($) {
+
+  /**
+   * Progress bar class api
+   */
+  var ProgressBarClass = function() {
+    this.initialize.apply(this, arguments);
+  };
+  $.extend(ProgressBarClass.prototype, {
+
+    initialize: function(el, config) {
+      $.extend(this, config);
+      this.el = el.toggleClass('rtl', this.rtl);
+      this.cssRange = createCssRangeArray(this.cssRange);
+      this.setValue(this.start);
+    },
+
+    setValue: function(newValue) {
+      this.value = newValue % (this.total + 1);
+      this.syncUI();
+    },
+
+    setTotal: function(newTotal) {
+      this.total = newTotal;
+      this.setValue(this.value);
+    },
+
+    syncUI: function() {
+      var percent = Math.round(this.value / this.total * 100);
+      if (jQuery.isNaN(percent)){
+        percent = 0;
+      }
+      //move bar
+      var height = this.el.height();
+      var totalWidth = this.el.width();
+      var barWidth = Math.round(percent / 100 * totalWidth);
+
+      $('.bar', this.el).width(barWidth).height(height);
+
+      var divPercent = $('.percent', this.el);
+      divPercent.html(percent+'%').height(height).css({'line-height': height + 'px'});
+      var pos = barWidth - divPercent.outerWidth(true);
+      if (pos < 0){
+        pos = 0;
+      }
+      divPercent.css(this.rtl ? {right: pos+'px'} : {left: pos+'px'});
+
+      for (var i = 0; i < this.cssRange.length; i++) {
+        var item = this.cssRange[i];
+        this.el.toggleClass(item.css, item.to >= percent && item.from <= percent);
+      }
+    }
+  });
+
+  /** helper functions */
+  function createUI(target) {
+    return $(
+      '<div class="progress-bar-container">' +
+        '<div class="bar"/>' +
+        '<div class="percent"/>' +
+      '</div>'
+    ).appendTo(target);
+  }
+
+  function createCssRangeArray(cssRangeObject) {
+    var arr = [];
+    for (var from in cssRangeObject) {
+      arr.push({from: parseInt(from, 10), css: cssRangeObject[from]});
+    }
+    arr.sort(function(a,b){
+      a = a.from; b = b.from;
+      return a < b ? -1 : a === b ? 0 : 1;
+    });
+    //calculate to value
+    for (var i = 1; i < arr.length; i++) {
+      var item = arr[i - 1];
+      item.to = arr[i].from - 1;
+    }
+    arr[arr.length - 1].to = 100;
+    return arr;
+  }
+
+  $.fn.progressBar = function(options) {
+    if (options) { //create new progress bar widgets
+      this.each(function() {
+        var target = $(this);
+        var config = $.extend($.fn.progressBar.defaults, options);
+        config.rtl = target.css('direction') == 'rtl';
+        target.data('api', new ProgressBarClass(createUI(target), config));
+      });
+      return this;
+    } else { //return existing instance api
+      return $(this).data('api');
+    }
+  };
+
+  $.fn.progressBar.defaults = {
+    start: 0,
+    total: 100,
+    cssRange: {0: 'red', 30: 'orange', 75: 'green'},
+    width: 200
+  };
+
+})(jQuery);
